cdcea8676020174b280d7b8525161a7a21b39a4e
DaryaGrygorova <grygorova.darya@gmail.com>
2022-08-10 16:01:31 +0300

    add Box

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,11 @@
+import { GlobalStyle } from './GlobalStyle';
+import { Box } from './Box';
+
 export const App = () => {
-  return <div>React homework template</div>;
+  return (
+    <Box>
+      <div>React homework template</div>
+      <GlobalStyle />
+    </Box>
+  );
 };
